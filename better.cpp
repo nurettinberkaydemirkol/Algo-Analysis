965eda06c5acdbed77862da7763bf000b7cc29f4
Nurettin Berkay Demirkol <90573081+nurettinberkaydemirkol@users.noreply.github.com>
2023-05-15 16:06:49 +0300

    Update better.cpp

--- better.cpp
+++ better.cpp
@@ -140,19 +140,16 @@ void function3(int g[][G_SIZE], int d[][G_SIZE], int size) {
         }
     }
 
-    // Her bir düğüm için "Adım Adım Dijkstra" algoritmasını uygulayın
+
     for (int src = 0; src < size; src++) {
-        // Ziyaret edilen düğümleri takip etmek için bir dizi oluşturun
         bool visited[size];
         memset(visited, false, sizeof(visited));
 
-        // Başlangıç düğümünü ziyaret edildi olarak işaretleyin
         visited[src] = true;
         d[src][src] = 0;
 
-        // src düğümünden tüm diğer düğümlere olan en kısa yolları hesaplayın
+
         for (int count = 0; count < size - 1; count++) {
-            // Henüz ziyaret edilmemiş en kısa yol düğümünü bulun
             int minDistance = INF;
             int minIndex = -1;
             for (int i = 0; i < size; i++) {
@@ -162,10 +159,8 @@ void function3(int g[][G_SIZE], int d[][G_SIZE], int size) {
                 }
             }
 
-            // En kısa yol düğümünü ziyaret edildi olarak işaretleyin
             visited[minIndex] = true;
 
-            // En kısa yol düğümünden geçerek diğer düğümlere olan yolları güncelleyin
             for (int i = 0; i < size; i++) {
                 if (!visited[i] && g[minIndex][i] && d[src][minIndex] + g[minIndex][i] < d[src][i]) {
                     d[src][i] = d[src][minIndex] + g[minIndex][i];
